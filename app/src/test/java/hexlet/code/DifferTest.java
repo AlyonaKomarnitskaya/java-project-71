cd5e7712e0b55f907de465b409d83216da6eed77
Alena Komarnitskaya <mixinny1@gmail.com>
2022-10-20 17:14:25 +0500

    fix tests

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -106,38 +106,38 @@ public class DifferTest {
     @Test
     public void testPlain1() throws Exception {
         String expected = """
-                Property 'chars2' was updated. From [complex value] to false
-                Property 'checked' was updated. From false to true
-                Property 'default' was updated. From null to [complex value]
-                Property 'id' was updated. From 45 to null
-                Property 'key1' was removed
-                Property 'key2' was added with value: 'value2'
-                Property 'numbers2' was updated. From [complex value] to [complex value]
-                Property 'numbers3' was removed
-                Property 'numbers4' was added with value: [complex value]
-                Property 'obj1' was added with value: [complex value]
-                Property 'setting1' was updated. From 'Some value' to 'Another value'
-                Property 'setting2' was updated. From 200 to 300
-                Property 'setting3' was updated. From true to 'none'""";
+                    Property 'chars2' was updated. From [complex value] to false
+                    Property 'checked' was updated. From false to true
+                    Property 'default' was updated. From null to [complex value]
+                    Property 'id' was updated. From 45 to null
+                    Property 'key1' was removed
+                    Property 'key2' was added with value: 'value2'
+                    Property 'numbers2' was updated. From [complex value] to [complex value]
+                    Property 'numbers3' was removed
+                    Property 'numbers4' was added with value: [complex value]
+                    Property 'obj1' was added with value: [complex value]
+                    Property 'setting1' was updated. From 'Some value' to 'Another value'
+                    Property 'setting2' was updated. From 200 to 300
+                    Property 'setting3' was updated. From true to 'none'""";
         assertEquals(expected, Differ.generate(path1, path2, "plain"));
     }
 
     @Test
     public void testPlain2() throws Exception {
         String expected = """
-              Property 'chars2' was updated. From [complex value] to false
-              Property 'checked' was updated. From false to true
-              Property 'default' was updated. From null to [complex value]
-              Property 'id' was updated. From 45 to null
-              Property 'key1' was removed
-              Property 'key2' was added with value: 'value2'
-              Property 'numbers2' was updated. From [complex value] to [complex value]
-              Property 'numbers3' was removed
-              Property 'numbers4' was added with value: [complex value]
-              Property 'obj1' was added with value: [complex value]
-              Property 'setting1' was updated. From 'Some value' to 'Another value'
-              Property 'setting2' was updated. From 200 to 300
-              Property 'setting3' was updated. From true to 'none'""";
+                  Property 'chars2' was updated. From [complex value] to false
+                  Property 'checked' was updated. From false to true
+                  Property 'default' was updated. From null to [complex value]
+                  Property 'id' was updated. From 45 to null
+                  Property 'key1' was removed
+                  Property 'key2' was added with value: 'value2'
+                  Property 'numbers2' was updated. From [complex value] to [complex value]
+                  Property 'numbers3' was removed
+                  Property 'numbers4' was added with value: [complex value]
+                  Property 'obj1' was added with value: [complex value]
+                  Property 'setting1' was updated. From 'Some value' to 'Another value'
+                  Property 'setting2' was updated. From 200 to 300
+                  Property 'setting3' was updated. From true to 'none'""";
         assertEquals(expected, Differ.generate(path3, path4, "plain"));
     }
 
